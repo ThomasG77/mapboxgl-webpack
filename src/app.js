0e3126422dc03ca6301166f70c1a51d12b756741
ThomasG77 <thomas_gratier@yahoo.fr>
2017-09-18 17:12:39 +0200

    Add map navigation controls

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ import './../node_modules/mapbox-gl/dist/mapbox-gl.css';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZXdpbGxpYW1zb24iLCJhIjoibzRCYUlGSSJ9.QGvlt6Opm5futGhE5i-1kw';
 
+// Instanciate a map object
 var map = new mapboxgl.Map({
     container: 'map', // container id
     style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
     center: [-74.50, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
+
