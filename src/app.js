5efcb6f200be226546911c45115570e7c249d325
ThomasG77 <thomas_gratier@yahoo.fr>
2017-09-18 02:41:22 +0200

    First import

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import mapboxgl from 'mapbox-gl';
+import './style.css';
+import '!css-loader!mapbox-gl/dist/mapbox-gl.css';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZXdpbGxpYW1zb24iLCJhIjoibzRCYUlGSSJ9.QGvlt6Opm5futGhE5i-1kw';
+
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+/*
+ * We've enabled commonsChunkPlugin for you. This allows your app to
+ * load faster and it splits the modules you provided as entries across
+ * different bundles!
+ *
+ * https://webpack.js.org/plugins/commons-chunk-plugin/
+ *
+ */
+
+/*
+ * We've enabled ExtractTextPlugin for you. This allows your app to
+ * use css modules that will be moved into a separate CSS file instead of inside
+ * one of your module entries!
+ *
+ * https://github.com/webpack-contrib/extract-text-webpack-plugin
+ *
+ */
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	entry: {
+		main: './src/app.js'
+	},
+
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		noParse: /(mapbox-gl)\.js$/,
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['es2015']
+				}
+			},
+			{
+				test: /\.css$/,
+				// use: ExtractTextPlugin.extract({
+				// 	fallback: "style-loader",
+				// 	use: "css-loader"
+				// })
+
+				use: [
+					'style-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+
+	plugins: [
+		// new ExtractTextPlugin("styles.css"),
+		new UglifyJSPlugin({
+			uglifyOptions: {
+				comparisons: false
+			}
+		})
+	]
+};
