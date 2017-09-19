a5d15a21d24157d1e08e5857331283071c13f32b
ThomasG77 <thomas_gratier@yahoo.fr>
2017-09-19 20:52:55 +0200

    Enable real url call to mapbox services for demo purpose

--- src/app.js
+++ src/app.js
@@ -7,7 +7,8 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzZzc3IiwiYSI6Im5Yc3p3SW8ifQ.FtzS4C8q_U
 // Instanciate a map object
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    // style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+    style: 'https://api.mapbox.com/styles/v1/mapbox/streets-v9?access_token=pk.eyJ1IjoibWlrZXdpbGxpYW1zb24iLCJhIjoibzRCYUlGSSJ9.QGvlt6Opm5futGhE5i-1kw',
     center: [-74.50, 40], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
